0fd7fe255dcbdd7f6ab2ea9ec47b34563c2bce8d
hugo-hebert <hugo.mike.hebert@gmail.com>
2016-12-26 20:17:35 +0100

    Added Game, Map & PathFinding classes

---
+++ src/Model/Game.java
@@ -0,0 +1,35 @@
+package Model;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by hugo on 12/26/2016.
+ */
+public class Game implements Runnable {
+
+    private Clock cl;
+
+    private Map map;
+
+    private int score;
+
+    private ArrayList<Station> stations;
+
+    private ArrayList<Passenger> passengers;
+
+    private ArrayList<Line> lines;
+
+    private ArrayList<Train> trains;
+
+    private ArrayList<Wagon> wagons;
+
+    public void run() {
+
+    }
+
+    public void start(){
+
+    }
+
+}
