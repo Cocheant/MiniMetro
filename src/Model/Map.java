8ecd272d105f1f47cefef7acd12685b1a87a954d
hugo-hebert <hugo.mike.hebert@gmail.com>
2016-12-26 20:17:51 +0100

    Added Game, Map & PathFinding classes

---
+++ src/Model/Map.java
@@ -0,0 +1,7 @@
+package Model;
+
+/**
+ * Created by hugo on 12/26/2016.
+ */
+public class Map {
+}
---
+++ src/Model/PathFinding.java
@@ -0,0 +1,12 @@
+package Model;
+
+/**
+ * Created by hugo on 12/26/2016.
+ */
+public class PathFinding implements Runnable {
+
+    public void run() {
+
+    }
+
+}
