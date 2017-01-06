39fce728676b58482a5ca82eaf1e84e7940bcb85
hugo-hebert <hugo.mike.hebert@gmail.com>
2017-01-06 10:00:21 +0100

    Added some game logic methods & attributes

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -1,5 +1,6 @@
 package Model;
 
+import java.awt.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
@@ -14,21 +15,43 @@ public class Game implements Runnable {
 
     private int score;
 
+    private int countStations;
     private ArrayList<Station> stations;
 
+    private int countPassengers;
     private ArrayList<Passenger> passengers;
 
+    private int availableLines;
+    private int countLines;
     private ArrayList<Line> lines;
 
+    private int availableTrains;
+    private int countTrains;
     private ArrayList<Train> trains;
 
+    private int availableWagons;
+    private int countWagons;
     private ArrayList<Wagon> wagons;
 
     /**
      * Default constructor
      */
     public Game(){
+
+        countStations = 0;
+        countPassengers = 0;
+        availableLines = 3;
+        countLines = 0;
+        availableTrains = 3;
+        countTrains = 0;
+        availableWagons = 0;
+        countWagons = 0;
+        score = 0;
         map = new Map();
+
+        addStation(new Coordinates(10,10), Shape.Circle);
+        addStation(new Coordinates(20,20), Shape.Circle);
+        addStation(new Coordinates(30,30), Shape.Circle);
     }
 
     public void run() {
@@ -39,6 +62,30 @@ public class Game implements Runnable {
 
     }
 
+    private void addStation(Coordinates co, Shape sh){
+        int id = countStations;
+        this.stations.add(new Station(co,id,sh));
+        this.countStations ++;
+    }
+
+    private void addPassenger(Shape sh){
+        int id = countPassengers;
+        this.passengers.add(new Passenger(sh, id));
+        this.countPassengers ++;
+    }
+
+    private void addLine(Color col, ArrayList<Station> stops) throws Exception{
+        if(countLines < availableLines){
+            int id = countLines;
+            this.lines.add(new Line(id, col, stops));
+        }else{
+            throw new Exception("No available lines");
+        }
+    }
+
+    private void modifyLine(int id, ArrayList<Station> stops){
+
+    }
 
 
 }
